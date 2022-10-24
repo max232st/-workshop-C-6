034236d31f869429be60a9bb80538cda67edd6db
max232st <max232st@gmail.com>
2022-10-24 12:14:30 +0300

    Семинар 6

---
+++ task006_dz/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+void Main()
+{
+    Console.Clear();
+    int[] array = NewArray();
+    int count = Count(array);
+    Console.WriteLine($"Кол-во элементов больше 0: {count}.");
+}
+
+int[] NewArray()
+{
+    Console.Write("Введите числа используя ',' как разделитель: ");
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+    Console.WriteLine(string.Join(" ", arr));
+    return arr;
+}
+
+int Count(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Main();
---
+++ task007_dz/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+void Main()
+{
+    Console.Clear();
+    Console.WriteLine($"Ведите число b1:");
+    double b1 = double.Parse(Console.ReadLine()!);
+    Console.WriteLine($"Ведите число k1:");
+    double k1 = double.Parse(Console.ReadLine()!);
+    Console.WriteLine($"Ведите число b2:");
+    double b2 = double.Parse(Console.ReadLine()!);
+    Console.WriteLine($"Ведите число k2:");
+    double k2 = double.Parse(Console.ReadLine()!);
+    CrossPoint(b1, k1, b2, k2);
+}
+void CrossPoint(double b1, double k1, double b2, double k2)
+{
+    if (b1 == b2 && k1 == k2)
+    {
+        Console.WriteLine("Прямые не пересекаются!!!");
+    }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k2 * x + b2;
+        Console.WriteLine($"Точка пересечения x = {x} y = {y}");
+    }
+}
+Main();
